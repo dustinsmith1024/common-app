8ecb8959bfba78f3697ea7dfc9efa13a85c4a41e
Dustin Smith <dustin.smith@c2fo.com>
2014-09-02 20:56:49 -0500

    adding note count and length

--- client/app/notes/notes.controller.js
+++ client/app/notes/notes.controller.js
@@ -4,8 +4,10 @@ angular.module('commonApp')
   .controller('NotesCtrl', function ($scope, $http) {
 
     $scope.note = 'Lead by example by taking greate notes.';
-    $scope.labels = ['books', 'reading'];
+    $scope.labels = ['business idea'];
     $scope.addingNote = false; //keeps add note hidden
+
+    $scope.added = 0;
 
     $http.get('/api/notes').success(function(notes){
         $scope.cards = notes;
@@ -15,6 +17,12 @@ angular.module('commonApp')
       $scope.addingNote = !$scope.addingNote;
     };
 
+    // This doesn't appear to be needed, but as is now it could be a performance issue
+    // $scope.$watch('note', function(noteText){
+    //   console.log(noteText);
+    //   $scope.noteLength = noteText.length;
+    // });
+
     $scope.addCard = function() {
       // assign to object for mongo'ing - could do on backend
       var labels = [];
@@ -22,14 +30,18 @@ angular.module('commonApp')
         labels.push({label: val});
       });
 
+      // move to promise of the .post
       $scope.cards.push({
         note: $scope.note,
         labels: labels
       });
 
+
       $http.post('/api/notes', {
         note: $scope.note,
         labels: labels
+      }).success(function(){
+        $scope.added++;
       });
 
       $scope.note = '';
